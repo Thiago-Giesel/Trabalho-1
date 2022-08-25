5cbe7597508cccf47de0b0719c4ee6aee131d734
Thiago-Giesel <112083035+Thiago-Giesel@users.noreply.github.com>
2022-08-25 09:29:29 -0300

    Add files via upload

---
+++ Trabalho 1.R
@@ -0,0 +1,15 @@
+#Thiago
+#Atividade sugerida pelo profesor wandre
+
+#Calcule o valor de X
+
+x <- c("a", "a", "b", "b", "b", "c" )
+table(x)
+2*x*2-x-4=0
+(-b + sqrt(b^2 - 4*a*c))/(2*a)
+a <-2; b <- -1; c <- -4
+(-b + sqrt(b^2 - 4*a*c))/(2*a)
+(-b - sqrt(b^2 - 4*a*c))/(2*a)
+
+
+#x=2+
